c3a47a947851002648d83bc10b9b1b10a867fe83
Khalal Walker <khalal.walker@gmail.com>
2019-07-29 23:25:04 -0700

    basic dad joke generator

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import TwilioRestClient
+import requests
+from dad_jokes import get_dad_joke
+
+app = Flask(__name__)
+
+
+@app.route('/dad', methods=['GET', 'POST'])
+def incoming_sms():
+    body = request.values.get('Body', None)
+
+    resp = MessagingResponse()
+
+    # determine message response
+    if body.lower() == 'dad':
+        resp.message(get_dad_joke())
+    else:
+        resp.message('Text "dad" to get a random dad joke')
+
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ dad_jokes.py
@@ -0,0 +1,7 @@
+import requests
+
+
+def get_dad_joke():
+    resp = requests.get('https://icanhazdadjoke.com/',
+                        headers={'Accept': 'text/plain'})
+    return resp.content.decode('utf-8')
